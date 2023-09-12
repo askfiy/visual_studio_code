68b0c5f8cae096ebba3d79064ef4b433fc9f6dd8
askfiy <c232318108@gmail.com>
2023-09-12 10:20:01 +0800

    chore(lualine): delete random colorsheme

--- lua/lualine/themes/visual_studio_code.lua
+++ lua/lualine/themes/visual_studio_code.lua
@@ -1,18 +1,8 @@
-math.randomseed(os.time())
-
-local random = math.random(0, 1)
-
 local colors = {}
 
-if random == 0 then
-    colors.foreground = "#FFFFFF"
-    colors.background = "#68217A"
-    colors.header_background = "#16825D"
-else
-    colors.foreground = "#FFFFFF"
-    colors.background = "#007ACC"
-    colors.header_background = "#2C896B"
-end
+colors.foreground = "#FFFFFF"
+colors.background = "#007ACC"
+colors.header_background = "#2C896B"
 
 local component_colors = {
     a = { fg = colors.foreground, bg = colors.header_background },
