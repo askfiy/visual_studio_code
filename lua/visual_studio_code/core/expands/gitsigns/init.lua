5dcee1198e7065c07efefa0befd0d3d6a317b35c
哪吒 <nezha@yizhoucp.com>
2024-06-21 15:53:51 +0800

    chore(gitsigns): Update color group

--- lua/visual_studio_code/core/expands/gitsigns/init.lua
+++ lua/visual_studio_code/core/expands/gitsigns/init.lua
@@ -5,14 +5,24 @@ function M.get_highlight(conf, colors)
     return {
         GitSignsCurrentLineBlame = { fg = colors.__vscode_inactive_table_foreground },
         GitSignsAdd = { fg = colors.__vscode_git_added_foreground },
+        GitSignsAddLn = { link = "GitSignsAdd" },
+        GitSignsAddNr = { link = "GitSignsAdd" },
         GitSignsChange = { fg = colors.__vscode_git_modified_foreground },
+        GitSignsChangeLn = { link = "GitSignsChange" },
+        GitSignsChangeNr = { link = "GitSignsChange" },
+        GitSignsChangeDelete = { fg = colors.__vscode_git_modified_foreground },
+        GitSignsChangeDeleteLn = { link = "GitSignsChange" },
+        GitSignsChangeDeleteNr = { link = "GitSignsChange" },
         GitSignsDelete = { fg = colors.__vscode_git_stage_deleted_foreground },
-        GitSignsAddLn = { fg = colors.__vscode_git_added_foreground },
-        GitSignsChangeLn = { fg = colors.__vscode_git_modified_foreground },
-        GitSignsDeleteLn = { fg = colors.__vscode_git_stage_deleted_foreground },
+        GitSignsDeleteLn = { link = "GitSignsDelete" },
+        GitSignsDeleteNr = { link = "GitSignsDelete" },
+        GitSignsTopdelete = { fg = colors.__vscode_git_stage_deleted_foreground },
+        GitSignsTopdeleteLn = { link = "GitSignsDelete" },
+        GitSignsTopdeleteNr = { link = "GitSignsDelete" },
         GitSignsAddInline = { fg = colors.__vscode_git_added_foreground },
         GitSignsDeleteInline = { fg = colors.__vscode_git_modified_foreground },
         GitSignsChangeInline = { fg = colors.__vscode_git_stage_deleted_foreground },
+
     }
 end
 
